eb0069467257d2bd0385b41e1f4cbe971363d3e7
Matan Shaviro <matan3sh@gmail.com>
2019-09-02 16:15:52 +0300

    adding videoPlayer Widget

--- lib/src/screens/portfolio_detail_screen.dart
+++ lib/src/screens/portfolio_detail_screen.dart
@@ -46,7 +46,10 @@ class PortfolioDetailScreenState extends State<PortfolioDetailScreen> {
                         alignment: Alignment.centerLeft,
                         child: Text(portfolio.description))),
                 ImagesCarousel(portfolio),
-                VideoDemo()
+                AspectRatio(
+                  aspectRatio: 3.2,
+                  child: VideoPlayerApp()
+                )
               ],
             )
           : Container(width: 0, height: 0),
@@ -209,32 +212,50 @@ class ImagesCarousel extends StatelessWidget {
   }
 }
 
-class VideoDemo extends StatefulWidget {
-  VideoDemo() : super();
-
-  final String title = "Video Demo";
-
+class VideoPlayerApp extends StatelessWidget {
   @override
-  VideoDemoState createState() => VideoDemoState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Player Demo',
+      home: VideoPlayerScreen(),
+    );
+  }
 }
 
-class VideoDemoState extends State<VideoDemo> {
+class VideoPlayerScreen extends StatefulWidget {
+  VideoPlayerScreen({Key key}) : super(key: key);
 
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   VideoPlayerController _controller;
   Future<void> _initializeVideoPlayerFuture;
 
   @override
   void initState() {
-    _controller = VideoPlayerController.network('https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    // Initialize the controller and store the Future for later use.
     _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
     _controller.setLooping(true);
-    _controller.setVolume(1.0);
+
     super.initState();
   }
 
   @override
   void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
     _controller.dispose();
+
     super.dispose();
   }
 
@@ -242,35 +263,47 @@ class VideoDemoState extends State<VideoDemo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Video Demo"),
+        title: Text('Butterfly Video'),
       ),
+      // Use a FutureBuilder to display a loading spinner while waiting for the
+      // VideoPlayerController to finish initializing.
       body: FutureBuilder(
         future: _initializeVideoPlayerFuture,
-        builder: (context, snapshot){
-          if(snapshot.connectionState == ConnectionState.done){
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the VideoPlayerController has finished initialization, use
+            // the data it provides to limit the aspect ratio of the video.
             return AspectRatio(
               aspectRatio: _controller.value.aspectRatio,
+              // Use the VideoPlayer widget to display the video.
               child: VideoPlayer(_controller),
             );
-          }else{
-            return Center(
-              child: CircularProgressIndicator(),
-            );
+          } else {
+            // If the VideoPlayerController is still initializing, show a
+            // loading spinner.
+            return Center(child: CircularProgressIndicator());
           }
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
           setState(() {
-            if(_controller.value.isPlaying) {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
               _controller.pause();
-            }else{
+            } else {
+              // If the video is paused, play it.
               _controller.play();
             }
           });
         },
-        child: Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
-      ),
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
